12c7afe4f95e52f7ac258264ae06147b38c544be
Apoorv Negi <apoorvnegi17@gmail.com>
2026-09-12 20:21:59 +0530

    fix: inline XML and plain-text responses for sitemap.xml and robots.txt to prevent SPA fallback

--- server.js
+++ server.js
@@ -96,13 +96,89 @@ const { addSubscriber, getAllSubscribers, sendDailyDigestEmails } = require('./s
 
 // Route handlers for search engines and SEO crawlers
 app.get('/sitemap.xml', (req, res) => {
-  res.header('Content-Type', 'application/xml; charset=utf-8');
-  res.sendFile(path.join(__dirname, 'sitemap.xml'));
+  res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+  return res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+  <url>
+    <loc>https://grevix.online/</loc>
+    <lastmod>2026-09-12</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://grevix.online/about</loc>
+    <lastmod>2026-09-12</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://grevix.online/projects</loc>
+    <lastmod>2026-09-12</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://grevix.online/events</loc>
+    <lastmod>2026-09-12</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://grevix.online/community</loc>
+    <lastmod>2026-09-12</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://grevix.online/blog</loc>
+    <lastmod>2026-09-12</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://grevix.online/privacy</loc>
+    <lastmod>2026-09-12</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
+  </url>
+  <url>
+    <loc>https://grevix.online/terms</loc>
+    <lastmod>2026-09-12</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
+  </url>
+</urlset>`);
 });
 
 app.get('/robots.txt', (req, res) => {
-  res.header('Content-Type', 'text/plain; charset=utf-8');
-  res.sendFile(path.join(__dirname, 'robots.txt'));
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  return res.status(200).send(`# Grevix Official Robots.txt
+# https://grevix.online
+
+User-agent: *
+Allow: /
+Allow: /about
+Allow: /projects
+Allow: /events
+Allow: /community
+Allow: /blog
+Allow: /privacy
+Allow: /terms
+Allow: /styles.css
+Allow: /main.js
+Allow: /favicon.jpg
+Allow: /favicon.svg
+
+# Disallow private and backend endpoints
+Disallow: /api/
+Disallow: /private_data/
+
+# Sitemap location
+Sitemap: https://grevix.online/sitemap.xml
+`);
 });
 
 app.get('/', (req, res) => {
